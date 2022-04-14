b72f9fb32c03c306e553cfcb41545ebe25c7bd9b
hebamahmoud98 <89314697+hebamahmoud98@users.noreply.github.com>
2022-04-14 02:39:51 +0200

    products details

--- client/src/components/Products/Products.js
+++ client/src/components/Products/Products.js
@@ -17,7 +17,7 @@ function Products(props) {
 
           <div className="product-desc">
             <p>{product.title}</p>
-            <span>${product.price}</span>
+            <span>{product.price}$</span>
           </div>
           <button>Add to cart</button>
         </div>
